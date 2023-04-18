1406c94bf98ab384d7fef7fe6d7367bbb253fcec
blackcode-creator <legacyitsolution@gmail.com>
2023-04-18 15:13:35 +0300

    sql operations

---
+++ create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employee(
+    e_id INT NOT NULL,
+    e_name VARCHAR(20),
+    e_salary int,
+    e_age int,
+    e_gender VARCHAR(20),
+    e_department VARCHAR(20)
+
+    primary key(e_id)
+    );
---
+++ inserting_values.sql
@@ -0,0 +1,6 @@
+insert into employee values( 1, "Samuel", 95000 , 45 , "Male", "Operations");
+insert into employee values( 2, "Bob" , 80000 , 21, "Male", "Support");
+insert into employee values( 3, "Anne" , 125000 , 25, "Female", "Analytics");
+insert into employee values( 4, "Julia" , 73000 , 30, "Female", "Analytics");
+insert into employee values( 5, "Matt" , 159000 , 33, "Male", "Sales");
+insert into employee values( 6, "Jeff" , 112000 , 27, "Male", "Operations");
